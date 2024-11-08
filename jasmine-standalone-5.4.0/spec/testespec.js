25d39cee3c96aa3ddc89d8073cb793df89b881e0
ginogueirasenai <124396295+ginogueirasenai@users.noreply.github.com>
2024-11-08 15:15:44 -0300

    Add files via upload

---
+++ jasmine-standalone-5.4.0/spec/testespec.js
@@ -0,0 +1,86 @@
+//Exercicios
+
+// //Exercício 1: Desenvolva uma aplicação que multiplique três números.
+// describe("Multiplicação de três números",
+//     function(){
+//         let a = 2;
+//         let b = 3;
+//         let c = 4;
+
+//         let resultado = a*b*c
+
+//         it('Resultado de ' + a + '*' + b + '*' + c + ' = ' + resultado, function(){
+//             expect(resultado).toBe(resultado)
+//         })
+//     })
+
+// Exercício 2:Desenvolva uma função dividir que divide dois números, garantindo que não haja
+// divisão por zero.
+// describe("Divisão de dois números", function() {
+//     let a = 8;
+//     let b = 0;
+//     let resultado = dividir(a, b);
+
+//     it('Resultado de ' + a + ' / ' + b + ' = ' + resultado, function() {
+//         expect(resultado).toBe(resultado);
+//     });
+
+// })
+
+// // Exercício 3: Crie uma função celsiusFahrenheit e outra fahrenheitCelsius
+// describe("Conversão de temperatura", function() {
+//     let celsius = 77;
+//     let fahrenheit = 77;
+
+//     it('Conversão de Celsius para Fahrenheit: ' + celsius + '°C = ' + celsiusFahrenheit(celsius) + '°F', function() {
+//         expect(celsiusFahrenheit(celsius)).toBe(celsiusFahrenheit(celsius));
+//     });
+
+//     it('Conversão de Fahrenheit para Celsius: ' + fahrenheit + '°F = ' + fahrenheitCelsius(fahrenheit) + '°C', function() {
+//         expect(fahrenheitCelsius(fahrenheit)).toBeCloseTo(fahrenheitCelsius(fahrenheit));
+//     });
+// });
+
+// // Exercício 5: Crie uma função contarCaracteres que retorna o número de caracteres em uma string.
+// describe("Contagem de caracteres em uma string", function() {
+//     let texto = "Giovanna";
+//     let numCaracteres = contarCaracteres(texto);
+
+//     it('Número de caracteres em "' + texto + '" = ' + numCaracteres, function() {
+//         expect(numCaracteres).toBe(numCaracteres);
+//     });
+// });
+
+// // Exercício 6: Crie uma função calculadora que realiza as quatro operações básicas.
+// describe("Calculadora básica", function() {
+//     let a = 10;
+//     let b = 0;
+
+//     it('Soma: ' + a + ' + ' + b + ' = ' + calculadora(a, b, '+'), function() {
+//         expect(calculadora(a, b, '+')).toBe(calculadora(a, b, '+'));
+//     });
+
+//     it('Subtração: ' + a + ' - ' + b + ' = ' + calculadora(a, b, '-'), function() {
+//         expect(calculadora(a, b, '-')).toBe(calculadora(a, b, '-'));
+//     });
+
+//     it('Multiplicação: ' + a + ' * ' + b + ' = ' + calculadora(a, b, '*'), function() {
+//         expect(calculadora(a, b, '*')).toBe(calculadora(a, b, '*'));
+//     });
+
+//     it('Divisão: ' + a + ' / ' + b + ' = ' + calculadora(a, b, '/'), function() {
+//         expect(calculadora(a, b, '/')).toBe(calculadora(a, b, '/'));
+//     });
+
+// });
+
+
+
+        
+         
+    
+                
+   
+
+
+   
---
+++ jasmine-standalone-5.4.0/src/teste.js
@@ -0,0 +1,44 @@
+// //Exercicios
+
+// //Exercício 1: Desenvolva uma aplicação que multiplique três números.
+// function multiplicarTresNumeros(resultado) {
+//     return resultado;
+// }
+
+// Exercício 2:Desenvolva uma função dividir que divide dois números, garantindo que não haja
+// divisão por zero.
+// function dividir(a, b) {
+//     if ((a === 0)|| (b ==0)) {
+//         return "Erro: Divisão por zero não permitida.";
+//     }
+//     return a / b;
+// }
+
+// // Exercício 3: Crie uma função celsiusFahrenheit e outra fahrenheitCelsius
+// function celsiusFahrenheit(c) {
+//     return c * 1.8 + 32;
+// }
+
+// function fahrenheitCelsius(f) {
+//     return (f - 32) / 1.8;
+// }
+
+// // Exercício 4: Desenvolva uma função calcularMedia que calcula a média de três números.
+// function calcularMedia(a, b, c) {
+//     return (a + b + c) / 3;
+// }
+
+// // Exercício 5: Crie uma função contarCaracteres que retorna o número de caracteres em uma string.
+// function contarCaracteres(texto) {
+//     return texto.length;
+// }
+
+// // Exercício 6: Crie uma função calculadora que realiza as quatro operações básicas (soma, subtração, multiplicação e divisão).
+// function calculadora(a, b, operador) {
+//     switch(operador) {
+//         case '+': return a + b;
+//         case '-': return a - b;
+//         case '*': return a * b;
+//         case '/': return (b === 0 || a === 0) ? "Erro: Divisão por zero não permitida." : a / b;
+//     }
+// }
